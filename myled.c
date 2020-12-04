c1c66dbddd9e1fb4ef5a6a0950e4c830028cb686
Tomoyayoshida <73051935+Tomoyayoshida@users.noreply.github.com>
2020-12-04 10:44:05 +0900

    Update myled.c

--- myled.c
+++ myled.c
@@ -1,3 +1,8 @@
+/* SPDX-License-Identifier: GPL-3.0 */
+/*
+ * Copyright (C) 2020 Ryuichi Ueda and Tomoya yoshida. All rights reserved.
+ */
+
 #include<linux/module.h>
 #include<linux/fs.h>
 #include<linux/cdev.h>
